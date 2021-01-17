54f1037fc808a1b1d9896aa759b65ea63512dfd8
thanhnhut02jan <thanhnhut.02jan@gmail.com>
2021-01-17 21:52:19 +0700

    function/markAsDoneBtn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 
 let listTodo = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
-
 let taskContainer = document.getElementById('task-container');
 
 function deleteTask(index) {
@@ -12,11 +11,27 @@ function deleteTask(index) {
     }
 }
 
+function markAsDoneTask(index) {
+    index = parseInt(index);
+    return function () { 
+        listTodo[index].isDone = !listTodo[index].isDone;
+        localStorage.setItem('tasks', JSON.stringify(listTodo));
+        render();
+    }
+}
+
 function render() {
     let mapList = listTodo.map((item, index) => {
-        return `<li class='list-item'>
-                    ${item}
-                    <div id='del-btn' class="delBtn" data-index=${index}><i class="far fa-trash-alt"></i></div>
+        let classList = 'list-item';
+        let icon = '<i class="fas fa-check"></i>';
+        if (item.isDone) {
+            classList += ' list-item--done';
+            icon = '<i class="fas fa-undo"></i>'
+        }
+        return `<li class='${classList}'>
+                    ${item.data}
+                    <div class="delBtn" data-index=${index}> <i class="far fa-trash-alt"></i> </i></div>
+                    <div class='markAsDoneBtn' data-index=${index}> ${icon} </div>
                 </li> 
         `
     }); 
@@ -29,23 +44,31 @@ function render() {
         let fn = deleteTask(item.getAttribute('data-index'));
         item.addEventListener('click', fn);
     });
-}
 
+    document.querySelectorAll('.markAsDoneBtn').forEach(item => {
+        let fn = markAsDoneTask(item.getAttribute('data-index'));
+        item.addEventListener('click', fn);
+    });
+}
 render();
 
 let input = document.getElementById('add-todo-input');
-
-function add(event) {
+function addTask(event) {
     if (event.which == 1 || event.which == 13) {
-        if (input.value) listTodo.push(input.value);
+        if (input.value) {
+            let Taskobj = {
+                data: input.value,
+                isDone: false
+            }
+            listTodo.push(Taskobj);
+        }
         localStorage.setItem('tasks', JSON.stringify(listTodo));
         render();
         input.value = '';
     }
 }
 
-
 let addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', add)
-input.addEventListener('keypress', add)
+addBtn.addEventListener('click', addTask);
+input.addEventListener('keypress', addTask);
     
